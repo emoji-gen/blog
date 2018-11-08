b4da7c98ffb867bfd5caff8ba8e4d838bb24893f
Pine Mizune <pinemz@gmail.com>
2018-11-09 01:17:34 +0900

    feat(pelicanconf.py): initial config

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,34 +4,63 @@
 from __future__ import unicode_literals
 from unidecode import unidecode
 
-AUTHOR = '絵文字ジェネレーター'
+
+#
+# Basic settings
+#
+USE_FOLDER_AS_CATEGORY = False
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
+DELETE_OUTPUT_DIRECTORY = True
+PATH = 'content'
 SITENAME = 'blog'
 SITEURL = ''
 
-PATH = 'content'
 
+#
+# URL settings
+#
+ARTICLE_URL = 'posts/{date:%Y%m%d}/{slug}.html'
+ARTICLE_SAVE_AS = 'posts/{date:%Y%m%d}/{slug}.html'
+CATEGORY_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+ARCHIVES_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+
+
+#
+# Time and Date
+#
 TIMEZONE = 'Asia/Tokyo'
+DEFAULT_DATE_FORMAT = '%Y/%m/%d'
 
-DEFAULT_LANG = 'ja'
 
-# Feed generation is usually not desired when developing
+#
+# Metadata
+#
+AUTHOR = '絵文字ジェネレーター'
+
+
+#
+# Feed settings
+#
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+#
+# Pagination
+#
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+
+#
+# Translations
+#
+DEFAULT_LANG = 'ja'
+
