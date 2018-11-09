3f2a2112ca1dd840973df487c63ad128096c11ed
Pine Mizune <pinemz@gmail.com>
2018-11-10 03:15:08 +0900

    feat(theme): update theme

--- content/test.md
+++ content/test.md
@@ -1,4 +1,4 @@
-Title: test
+Title: ああああああ、いいいいいいいいいいいいいいい、ううううううううううううううううううううううう
 Date: 2018-11-09 10:00
 Slug: test1
 Thumbnail: foooo
--- theme/src/components/index.ts
+++ theme/src/components/index.ts
@@ -1,6 +1,8 @@
 'use strict'
 
 import './organisms/header.scss'
+import './organisms/sidebar.scss'
+import './organisms/summary.scss'
 import './pages/blog.scss'
 import './templates/article.scss'
 import './templates/index.scss'
