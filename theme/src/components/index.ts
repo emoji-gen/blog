e2dffdaed87c47d1a55f7d4c28deeab846d06a1a
Pine Mizune <pinemz@gmail.com>
2018-11-23 16:03:53 +0900

    feat(theme): add footer

--- theme/src/components/index.ts
+++ theme/src/components/index.ts
@@ -1,6 +1,7 @@
 'use strict'
 
 import './organisms/background.scss'
+import './organisms/footer.scss'
 import './organisms/header.scss'
 import './organisms/sidebar.scss'
 import './organisms/summary.scss'
