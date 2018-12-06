a42ed0fe8694ab3a19c75f78edc99e9c5a8c1a47
Pine Mizune <pinemz@gmail.com>
2018-12-06 18:38:24 +0900

    feat(scripts): add notify script

---
+++ scripts/notify.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import slackweb
+
+circleci_icon = 'https://i.imgur.com/FLjAA35.png'
+firebase_mini_icon = 'https://i.imgur.com/FYFxjXl.png'
+
+
+def main():
+    message = sys.argv[1]
+    if message == 'started':
+        _notify('Deploy started', '#66d3e4')
+    elif message == 'successful':
+        _notify('Deploy successful', '#41aa58')
+    elif message == 'failed':
+        _notify('Deploy failed', '#d10c20')
+    else:
+        raise RuntimeError('invalid message')
+
+
+def _notify(text, color):
+    if 'CIRCLE_BUILD_URL' in os.environ:
+        text += ' <{}|#{}>'.format(
+            os.environ['CIRCLE_BUILD_URL'],
+            os.environ['CIRCLE_BUILD_NUM']
+        )
+
+    slack = slackweb.Slack(url=os.environ['SLACK_INCOMING_WEBHOOK'])
+    attachment = {
+        'color': color,
+        'text': text,
+        'author_name': 'blog',
+        'author_icon': firebase_mini_icon,
+    }
+    slack.notify(
+        username='CircleCI',
+        icon_url=circleci_icon,
+        attachments=[attachment]
+    )
+
+
+if __name__ == '__main__':
+    main()
